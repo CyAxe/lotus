073f4174fcbb36863014bd7a9e28317069d65798
Khaled Nassar <knassar702@gmail.com>
2022-09-16 00:07:56 +0200

    Using clap instead

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,68 @@
 use lotus::Lotus;
-use structopt::StructOpt;
+use clap::{App, Arg, ArgMatches};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     init_log().unwrap();
-    let cmd_opts = Opt::from_args();
-    let lua_code = cmd_opts.scripts;
+    let cmd_opts = cmd_args();
+    let lua_code = cmd_opts.value_of("scripts").unwrap();
     let lottas = Lotus::init(lua_code.to_string());
     lottas
         .start(
-            cmd_opts.threads,
-            cmd_opts.script_threads,
-            &cmd_opts.json_output,
+            cmd_opts.value_of("workers").unwrap().trim().parse::<usize>().unwrap(),
+            cmd_opts.value_of("script_threads").unwrap().trim().parse::<usize>().unwrap(),
+            &cmd_opts.value_of("output").unwrap(),
         )
         .await;
     Ok(())
 }
 
-#[derive(Debug, StructOpt)]
-#[structopt(
-    name = "lotus",
-    about = "Fast Web Security Scanner written in Rust based on Lua Scripts "
-)]
 
-struct Opt {
-    #[structopt(
-        short = "w",
-        long = "workers",
-        default_value = "30",
-        help = "number of workers"
-    )]
-    threads: usize,
-    #[structopt(
-        short = "t",
-        long = "script-threads",
-        default_value = "5",
-        help = " Workers for lua scripts"
-    )]
-    script_threads: usize,
-    #[structopt(short = "s", long = "scripts", help = "Path of Scripts dir")]
-    scripts: String,
-    #[structopt(short = "o", long = "output", help = "Path of output JSON file")]
-    json_output: String,
+pub fn cmd_args() -> ArgMatches {
+    App::new("Lotus")
+        .version("0.1-beta")
+        .author("Khaled Nassar <knassar702@gmail.com>")
+        .about("Fast Web Security Scanner written in Rust based on Lua Scripts ")
+        .arg(
+            Arg::with_name("workers")
+                .help("Number of works of urls")
+                .required(true)
+                .short('w')
+                .takes_value(true)
+                .default_value("10")
+                .long("workers"),
+        )
+        .arg(
+            Arg::with_name("script_threads")
+            .help("Workers for lua scripts")
+            .short('t')
+            .long("script-threads")
+            .takes_value(true)
+            .default_value("5")
+            )
+
+        .arg(
+            Arg::with_name("scripts")
+            .help("Path of scripts dir")
+            .takes_value(true)
+            .short('s')
+            .long("scripts")
+            .required(true)
+            )
+
+        .arg(
+            Arg::with_name("output")
+            .help("Path of the JSON output fiel")
+            .required(true)
+            .takes_value(true)
+            .long("output")
+            .short('o')
+            )
+        .arg(
+            Arg::with_name("nolog")
+                .help("no logging")
+            )
+        .get_matches()
 }
 
 fn init_log() -> Result<(), std::io::Error> {
