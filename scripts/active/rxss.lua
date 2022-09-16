bb4268d18bfc53cd19e8844c7d750e1d3f98a935
Khaled Nassar <knassar702@gmail.com>
2022-09-16 17:35:06 +0200

    adding rxss output to the json report

--- scripts/active/rxss.lua
+++ scripts/active/rxss.lua
@@ -13,9 +13,15 @@ local function main(url)
             return
         end
         local css_pattern = generate_css_selector(PAYLOAD)
-        local searcher = html_search(body,css_pattern)
-        if string.len(searcher) > 0 then
-            println(string.format("RXSS: %s | %s | %s ",resp.url:GetStrOrNil(),PAYLOAD,css_pattern))
+        if string.len(css_pattern) > 0 then
+            local searcher = html_search(body,css_pattern)
+            if string.len(searcher) > 0 then
+                println(string.format("RXSS: %s | %s | %s ",resp.url:GetStrOrNil(),PAYLOAD,css_pattern))
+                REPORT["url"] = new_url
+                REPORT["match"] = css_pattern
+                REPORT["payload"] = PAYLOAD
+                VALID = true
+            end
         end
     end
     return REPORT
