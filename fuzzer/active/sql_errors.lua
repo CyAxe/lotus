d4ea6b5325db29e730da72cbe15522433f2c7083
Khaled Nassar <knassar702@gmail.com>
2022-11-01 14:46:19 +0200

    break for one results

--- fuzzer/active/sql_errors.lua
+++ fuzzer/active/sql_errors.lua
@@ -34,18 +34,23 @@ function main(url)
         log_error(log_msg)
         return
     end
-    for payload_index, payload in pairs(PAYLOADS) do 
-        local new_query = HttpMessage:setAllParams(payload)
-        for parameter_name, full_url in pairs(new_query) do
-            local resp = http:send("GET",full_url)
+    for param_index, param_name in pairs(HttpMessage:getParams()) do
+        STOP_PARAM = false
+        for payload_index, payload in pairs(PAYLOADS) do 
+            local new_url = HttpMessage:setParam(param_name,payload)
+            local resp = http:send("GET",new_url)
             local body = resp.body:GetStrOrNil()
+            if STOP_PARAM == true then
+                break
+            end
             for sqlerror_match in SQLI_ERRORS:gmatch("[^\n]+") do
                     local match = is_match(sqlerror_match,body)
                     if ( match == false or match == nil) then
                             -- NOTHING
                     else
-                        send_report(resp.url:GetStrOrNil(),parameter_name,payload,sqlerror_match)
+                        send_report(resp.url:GetStrOrNil(),param_name,payload,sqlerror_match)
                         Reports:addReport(NewReport)
+                        STOP_PARAM = true
                         break
                     end
             end
