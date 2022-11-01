4b6eeb59b81e513ecaf163b46f14a3b486318861
Khaled Nassar <knassar702@gmail.com>
2022-11-01 13:37:31 +0200

    adding comments

--- src/core/utils/bar.rs
+++ src/core/utils/bar.rs
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Lotus Project, an Web Security Scanner written in Rust based on Lua Scripts
+ * For details, please see https://github.com/rusty-sec/lotus/
+ *
+ * Copyright (c) 2022 - Khaled Nassar
+ *
+ * Please note that this file was originally released under the
+ * GNU General Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 use indicatif::{ProgressBar, ProgressStyle};
 
 /// Lotus ProgressBar based on the length of `bar` parameter
