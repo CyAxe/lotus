810e7ad9815736cbbf3ba8b65fb9c5577cf792dd
Khaled Nassar <knassar702@gmail.com>
2023-03-14 01:38:01 +0200

    Change logging messages

--- src/lua/run.rs
+++ src/lua/run.rs
@@ -76,15 +76,8 @@ impl LuaLoader {
     /// * `target_type` - the input type if its HOST or URL
     pub async fn run_scan<'a>(&self, lua_opts: LuaOptions<'_>) -> Result<(), mlua::Error> {
         let lua = Lua::new();
-        // settings lua api
-        if let ScanTypes::HOSTS = lua_opts.target_type {
-            self.set_lua(None, &lua);
-            lua.globals()
-                .set("TARGET_HOST", lua_opts.target_url.unwrap())
-                .unwrap();
-        } else {
-            self.set_lua(lua_opts.target_url, &lua);
-        }
+
+        // set lua globals
         lua.globals()
             .set("SCRIPT_PATH", lua_opts.script_dir)
             .unwrap();
@@ -92,62 +85,63 @@ impl LuaLoader {
             .set("FUZZ_WORKERS", lua_opts.fuzz_workers)
             .unwrap();
 
-        // Handle this error please
+        match lua_opts.target_type {
+            ScanTypes::HOSTS => {
+                // for HOSTS, set TARGET_HOST global
+                self.set_lua(None, &lua);
+                lua.globals()
+                    .set("TARGET_HOST", lua_opts.target_url.unwrap())
+                    .unwrap();
+            }
+            _ => {
+                // for all other target types, set target URL
+                self.set_lua(lua_opts.target_url, &lua);
+            }
+        };
+
+        // execute script code
         let run_code = lua.load(lua_opts.script_code).exec_async().await;
-        if run_code.is_err() {
-            {
-                let bar = BAR.lock().unwrap();
-                bar.inc(1);
-                bar.println("Script Error")
-            };
-            return run_code;
+        if let Err(e) = run_code {
+            let bar = BAR.lock().unwrap();
+            let error_msg = format!("An error occurred while running the script:\n\n{}\n\nPlease check the script code and try again.", e);
+            bar.inc(1);
+            bar.println(&error_msg);
+            return Err(e);
         }
+
+        // call main function
         let main_func = lua.globals().get::<_, mlua::Function>("main");
-        if main_func.is_err() {
-            log::error!(
-                "[{}] there is no main function, Skipping ..",
-                lua_opts.script_dir
-            );
-            {
-                BAR.lock().unwrap().println(format!(
-                    "[{}] there is no main function, Skipping ..",
-                    lua_opts.script_dir
-                ))
-            };
+        if let Err(_) = main_func {
+            let msg = format!("The script in directory [{}] does not contain a main function.\n\nThe main function is required to execute the script. Please make sure that the script contains a main function and try again.", lua_opts.script_dir);
+            log::error!("{}", msg);
+            BAR.lock().unwrap().println(msg);
+            return Ok(());
+        }
+
+        let run_scan = main_func.unwrap().call_async::<_, mlua::Value>(mlua::Value::Nil).await;
+        BAR.lock().unwrap().inc(1);
+
+        if let Err(e) = run_scan {
+            let msg = format!("[{}] Script Error: {:?}", lua_opts.script_dir, e);
+            log::error!("{}", msg);
+            BAR.lock().unwrap().println(msg);
         } else {
-            let run_scan = main_func
-                .unwrap()
-                .call_async::<_, mlua::Value>(mlua::Value::Nil)
-                .await;
-            {
-                BAR.lock().unwrap().inc(1)
-            };
-            if run_scan.is_err() {
-                log::error!(
-                    "[{}] Script Error : {:?}",
+            // process script report
+            let script_report = lua.globals().get::<_, AllReports>("Reports").unwrap();
+            if !script_report.reports.is_empty() {
+                let results = serde_json::to_string(&script_report.reports).unwrap();
+                let report_count = script_report.reports.len();
+                let log_message = format!(
+                    "The script in directory [{}] generated {} report(s):\n\n{}\n\n",
                     lua_opts.script_dir,
-                    run_scan.clone().unwrap_err()
+                    report_count,
+                    results
                 );
-                {
-                    BAR.lock()
-                        .unwrap()
-                        .println(format!("Script ERROR: {:?}", run_scan.unwrap_err()))
-                };
-            } else {
-                let script_report = lua.globals().get::<_, AllReports>("Reports").unwrap();
-                if !script_report.reports.is_empty() {
-                    let results = serde_json::to_string(&script_report.reports).unwrap();
-                    log::debug!(
-                        "[{}] Report Length {}",
-                        lua_opts.script_dir,
-                        script_report.reports.len()
-                    );
-                    self.write_report(&results);
-                } else {
-                    log::debug!("[{}] Script report is empty", lua_opts.script_dir);
-                }
+                log::debug!("{}", log_message);
+                self.write_report(&results);
             }
         }
-        Ok(())
+
+    Ok(())
     }
 }
