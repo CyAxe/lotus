5cd910542236615b408c3df8a629560171ef9ad9
Khaled Nassar <knassar702@gmail.com>
2023-03-16 21:46:55 +0200

    Updating tests for http

--- tests/http.rs
+++ tests/http.rs
@@ -1,14 +1,20 @@
-use reqwest::header::{HeaderMap, HeaderValue};
 use lotus::lua::network::http::Sender;
+use reqwest::header::{HeaderMap, HeaderValue};
 
 #[tokio::test]
 async fn test_send() {
-
     // Create test sender
     let sender = Sender::init(HeaderMap::new(), None, 10, 3);
 
     // Send test request
-    let resp = sender.send("GET", "https://httpbin.org/get".to_string(), "".to_string(), HeaderMap::new()).await;
+    let resp = sender
+        .send(
+            "GET",
+            "https://httpbin.org/get".to_string(),
+            None,
+            sender.clone()
+        )
+        .await;
     assert!(resp.is_ok());
 
     // Check response body
@@ -22,10 +28,18 @@ fn test_init() {
     let headers = HeaderMap::new();
 
     // Create test sender
-    let sender = Sender::init(headers, Some("http://proxy.example.com:8080".to_string()), 10, 3);
+    let sender = Sender::init(
+        headers,
+        Some("http://proxy.example.com:8080".to_string()),
+        10,
+        3,
+    );
 
     // Check sender properties
-    assert_eq!(sender.proxy, Some("http://proxy.example.com:8080".to_string()));
+    assert_eq!(
+        sender.proxy,
+        Some("http://proxy.example.com:8080".to_string())
+    );
     assert_eq!(sender.timeout, 10);
     assert_eq!(sender.redirects, 3);
 }
@@ -37,9 +51,17 @@ fn test_build_client() {
     headers.insert("Content-Type", HeaderValue::from_static("application/json"));
 
     // Create test sender
-    let sender = Sender::init(headers.clone(), Some("http://proxy.example.com:8080".to_string()), 10, 3);
+    let sender = Sender::init(
+        headers.clone(),
+        Some("http://proxy.example.com:8080".to_string()),
+        10,
+        3,
+    );
 
-    assert_eq!(sender.proxy.unwrap().as_str(), "http://proxy.example.com:8080");
+    assert_eq!(
+        sender.proxy.unwrap().as_str(),
+        "http://proxy.example.com:8080"
+    );
 
     // Test no-proxy client builder
     let sender = Sender::init(headers.clone(), None, 10, 3);
