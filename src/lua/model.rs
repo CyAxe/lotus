c6d7185603c1a759ae98b2664b1e5b445c2b2a7e
Khaled Nassar <knassar702@gmail.com>
2023-04-03 03:34:26 +0200

    Rename loader to model

---
+++ src/lua/model.rs
@@ -0,0 +1,30 @@
+// This file is part of Lotus Project, a web security scanner written in Rust based on Lua scripts.
+// For details, please see https://github.com/rusty-sec/lotus/
+//
+// Copyright (c) 2022 - Khaled Nassar
+//
+// Please note that this file was originally released under the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+use crate::ScanTypes;
+use mlua::Lua;
+
+/// Setup The Lua Runtime
+pub struct LuaRunTime<'lua> {
+    pub lua: &'lua Lua,
+}
+
+pub struct LuaOptions<'a> {
+    pub target_url: Option<&'a str>,
+    pub target_type: ScanTypes,
+    pub fuzz_workers: usize,
+    pub script_code: &'a str,
+    pub script_dir: &'a str,
+    pub env_vars: serde_json::Value,
+}
