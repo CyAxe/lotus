293ae52c76e0b9f8890edad5ec9f75307c12c56b
Khaled Nassar <knassar702@gmail.com>
2023-04-12 08:42:40 +0200

    Adding lotus functions for input-handler

--- src/cli/startup/scan/input_handler.rs
+++ src/cli/startup/scan/input_handler.rs
@@ -1,14 +1,22 @@
 use mlua::{Lua, StdLib};
+use crate::lua::runtime::http_ext::HTTPEXT;
+use crate::lua::runtime::utils_ext::UtilsEXT;
+use crate::lua::model::LuaRunTime;
 use serde_json::Value as JsonValue;
 
 pub fn custom_input_lua(
     input_data: Vec<String>,
     code: &str,
 ) -> Result<Vec<JsonValue>, mlua::Error> {
-    let lua_env = unsafe { Lua::unsafe_new_with(StdLib::ALL_SAFE, mlua::LuaOptions::new()) };
+    let lua_env = LuaRunTime {
+        lua: &unsafe { Lua::unsafe_new_with(StdLib::ALL_SAFE, mlua::LuaOptions::new()) }
+    };
+    lua_env.add_printfunc();
+    lua_env.add_matchingfunc();
+    lua_env.add_httpfuncs(None);
     log::debug!("Loading Input handler lua code");
-    lua_env.load(code).exec()?;
-    let parse_input = lua_env.globals().get::<_, mlua::Function>("parse_input")?;
+    lua_env.lua.load(code).exec()?;
+    let parse_input = lua_env.lua.globals().get::<_, mlua::Function>("parse_input")?;
     log::debug!("Calling parse_input function");
     let output_data = parse_input.call::<_, Vec<mlua::Value>>(input_data)?;
     let final_output: Vec<JsonValue> = output_data
