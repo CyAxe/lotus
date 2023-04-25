d6c692efb726f3a795fe095f607bf0e78872bb16
Khaled Nassar <knassar702@gmail.com>
2023-04-25 07:48:53 +0200

    Add reason & redirect check

--- src/lua/network/http.rs
+++ src/lua/network/http.rs
@@ -35,6 +35,8 @@ lazy_static! {
 
 #[derive(Debug, FromToLua, TypeName)]
 pub struct HttpResponse {
+    pub is_redirect: bool,
+    pub reason: String,
     pub url: String,
     pub status: i32,
     pub body: String,
@@ -207,13 +209,17 @@ impl Sender {
                     .collect::<HashMap<String, String>>();
 
                 let url = resp.url().to_string();
+                let reason = resp.status().is_redirection();
                 let status = resp.status().as_u16() as i32;
+                let is_redirect = resp.status().is_redirection();
                 let body = resp
                     .bytes()
                     .await
                     .map(|b| String::from_utf8_lossy(&b).to_string());
                 match body {
                     Ok(body) => Ok(HttpResponse {
+                        is_redirect,
+                        reason,
                         url,
                         status,
                         body,
