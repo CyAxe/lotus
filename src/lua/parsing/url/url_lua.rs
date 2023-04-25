aacb77cd3d367804343d185786ebfffcdc100f39
Khaled Nassar <knassar702@gmail.com>
2023-04-25 22:30:49 +0200

    Updating HttpMessage functions name

--- src/lua/parsing/url/url_lua.rs
+++ src/lua/parsing/url/url_lua.rs
@@ -4,7 +4,11 @@ use url::Url;
 
 impl UserData for HttpMessage {
     fn add_methods<'lua, M: mlua::UserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_method_mut("setUrl", |_, this, url: String| match Url::parse(&url) {
+        methods.add_method("clone", |_,this,()|{
+            let msg = this.clone();
+            Ok(msg)
+        });
+        methods.add_method_mut("new", |_, this, url: String| match Url::parse(&url) {
             Ok(parsed_url) => {
                 this.url = Some(parsed_url.clone());
                 Ok(parsed_url.to_string())
@@ -12,30 +16,30 @@ impl UserData for HttpMessage {
             Err(err) => Err(err.to_lua_err()),
         });
         methods.add_method(
-            "setParam",
+            "param_set",
             |_, this, (param, payload, remove_content): (String, String, bool)| {
                 Ok(this.set_urlvalue(&param, &payload, remove_content))
             },
         );
         methods.add_method(
-            "setAllParams",
+            "param_set_all",
             |_, this, (payload, remove_content): (String, bool)| {
                 Ok(this.change_urlquery(&payload, remove_content))
             },
         );
-        methods.add_method("Url", |_, this, ()| match &this.url {
+        methods.add_method("url", |_, this, ()| match &this.url {
             Some(url) => Ok(url.as_str().to_string()),
             None => Err("No url found").to_lua_err(),
         });
-        methods.add_method("Path", |_, this, ()| match &this.url {
+        methods.add_method("path", |_, this, ()| match &this.url {
             Some(url) => Ok(url.path().to_string()),
             None => Err("No url found").to_lua_err(),
         });
-        methods.add_method("TxtParams", |_, this, ()| match &this.url {
+        methods.add_method("param_str", |_, this, ()| match &this.url {
             Some(url) => Ok(url.query().unwrap_or("").to_string()),
             None => Err("No url found").to_lua_err(),
         });
-        methods.add_method("Params", |_, this, ()| {
+        methods.add_method("param_list", |_, this, ()| {
             let mut all_params = Vec::new();
             match &this.url {
                 Some(url) => {
@@ -47,7 +51,7 @@ impl UserData for HttpMessage {
                 None => Err("No url found").to_lua_err(),
             }
         });
-        methods.add_method("urlJoin", |_, this, new_path: String| {
+        methods.add_method("urljoin", |_, this, new_path: String| {
             Ok(this.urljoin(new_path.as_str()))
         });
     }
